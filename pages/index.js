4232ce9dc4c05b683c81006893ad33ef429e0a5c
Tai Zeng <zengtai@gmail.com>
2022-07-23 11:13:35 +0800

    完成文章内图片地址抓取功能

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,22 @@
 import { getLocalData, resizeImage, removeLink } from "../lib/api";
 
 export default function Home({ data }) {
-  // console.log(`posts`, data.posts);
-  console.log(`imageUrls`, data.imageUrls);
+  console.log(`posts`, data.posts);
+  // console.log(`imageUrls`, data.imageUrls);
   // console.log(`categories`, data.categories);
+
+  let posts = data.posts;
+  let imgUrls = [];
 
   return (
     <>
       <div className="container mx-auto">
         <h1>This is homepage</h1>
-        {/* {posts.map((post) => {
+        {posts.map((post) => {
+          let srcUrl = post.content
+            .match(/src=[\'\"]?(?<url>[^\'\"]*)?[\'\"]/gim)
+            .map((i) => i.replace(/(src=)|\"/g, ""));
+          imgUrls = imgUrls.concat(srcUrl);
           return (
             <article className="article" key={post.id}>
               <h2 className="m-4 font-bold">
@@ -19,13 +26,21 @@ export default function Home({ data }) {
                 src={resizeImage(post.featured_media.url)}
                 alt={post.featured_media.id}
               />
-              <div
+              <div>{post.content}</div>
+              {/* <div
                 className="m-4 bg-slate-100 p-4"
                 dangerouslySetInnerHTML={{ __html: removeLink(post.content) }}
-              />
+              /> */}
             </article>
           );
+        })}
+        {/* {imgUrls.map((i) => {
+          i = i.replace(/src=/g, "");
         })} */}
+        {console.log(imgUrls)}
+        {imgUrls.map((i, index) => (
+          <img key={index} src={i} />
+        ))}
       </div>
     </>
   );
@@ -35,18 +50,13 @@ export const getStaticProps = async (ctx) => {
   // const categories = await getLocalData(`categories`);
   const posts = await getLocalData(`posts`);
   // const posts = await getLocalData(`posts`).then((res) => res.slice(0, 10));
-  let regex = `src\s*=\s*"(.+?)"`;
-  let imageUrls = [];
-  posts.map((post) =>
-    post.content.match(regex) ? imageUrls.push(post.content.match(regex)) : null
-  );
 
   return {
     props: {
       data: {
         // categories,
         // posts: posts ? posts : `Nothing`,
-        imageUrls,
+        posts: posts.slice(0, 1),
       },
     },
   };
